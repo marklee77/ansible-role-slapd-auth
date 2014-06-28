325f0c9735766852a696cb76936011920816ee54
Mark Stillwell <marklee@fortawesome.org>
2014-06-28 21:48:03 +0100

    temporarily added back file here, need to org filter plugins into different git repo...

--- filter_plugins/password_hash.py
+++ filter_plugins/password_hash.py
@@ -1,1 +1,14 @@
-../roles/marklee77.modules-extra/filter_plugins/password_hash.py
+def ssha_password_hash(*a, **kw):
+    from base64 import b64encode
+    from hashlib import sha1
+    password = a[0]
+    salt = a[1]
+    pw_hash = sha1(password + salt).digest()
+    return '{SSHA}' + b64encode("{}{}".format(pw_hash, salt))
+
+class FilterModule(object):
+    ''' utility filter hash passwords '''
+
+    def filters(self):
+        return { 'ssha_password_hash' : ssha_password_hash }
+
